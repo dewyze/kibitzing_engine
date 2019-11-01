e9399976ff782643fc65e69bd1f865cba61d4d84
John DeWyze <john.dewyze@getbraintree.com>
2019-11-01 12:11:33 -0500

    Move node bids under convention module

--- lib/kibitzing/engine/convention.ex
+++ lib/kibitzing/engine/convention.ex
@@ -4,7 +4,35 @@ defmodule Kibitzing.Engine.Convention do
             description: nil,
             nodes: []
 
+  alias Kibitzing.Engine.Convention.Requirement.Bid
+  alias Kibitzing.Engine.Convention.Node
+
   def new(id, name, description \\ nil) do
     %__MODULE__{id: id, name: name, description: description, nodes: []}
+  end
+
+  def bid(convention, requirements, options \\ Keyword.new()) do
+    node(convention, :bid, requirements, options)
+  end
+
+  def one_of(convention, requirements, options \\ Keyword.new()) do
+    node(convention, :one_of, requirements, options)
+  end
+
+  def pass(convention) do
+    node(convention, :bid, [&Bid.pass/1])
+  end
+
+  def double(convention) do
+    node(convention, :bid, [&Bid.double/1])
+  end
+
+  def redouble(convention) do
+    node(convention, :bid, [&Bid.redouble/1])
+  end
+
+  defp node(convention, method, requirements, options \\ Keyword.new()) do
+    node = Node.new(method, requirements, options)
+    %{convention | nodes: convention.nodes ++ [node]}
   end
 end
--- test/kibitzing/engine/convention_test.exs
+++ test/kibitzing/engine/convention_test.exs
@@ -1,8 +1,8 @@
 defmodule Kibitzing.Engine.ConventionTest do
   use ExUnit.Case
+  use ExUnitProperties
   # doctest Kibitzing.Engine.Convention
-
-  alias Kibitzing.Engine.Convention
+  alias Kibitzing.Engine.{Convention, Convention.Node, Convention.Requirement.Bid}
 
   describe "new" do
     test "creates a convention" do
@@ -11,6 +11,59 @@ defmodule Kibitzing.Engine.ConventionTest do
       assert convention.id == :two_over_one
       assert convention.name == "2/1"
       assert convention.description == "2/1 Opening"
+    end
+  end
+
+  describe "#bid" do
+    test "returns the convention with the added node" do
+      check all(thing <- term()) do
+        convention = Convention.new(:conv, "conv")
+
+        assert match?(
+                 %Convention{nodes: [%Node{method: :bid, requirements: [^thing]}]},
+                 Convention.bid(convention, [thing])
+               )
+      end
+    end
+  end
+
+  describe "#one_of" do
+    test "returns the convention with the added node" do
+      check all(
+              thing_1 <- term(),
+              thing_2 <- term()
+            ) do
+        convention = Convention.new(:conv, "conv")
+
+        assert match?(
+                 %Convention{nodes: [%Node{method: :one_of, requirements: [^thing_1, ^thing_2]}]},
+                 Convention.one_of(convention, [thing_1, thing_2])
+               )
+      end
+    end
+  end
+
+  describe "#pass" do
+    test "returns the convention with a node with a pass requirements" do
+      convention = Convention.pass(Convention.new(:conv, "conv"))
+      node = hd(convention.nodes)
+      assert node.requirements == [&Bid.pass/1]
+    end
+  end
+
+  describe "#double" do
+    test "returns the convention with a node with a double requirements" do
+      convention = Convention.double(Convention.new(:conv, "conv"))
+      node = hd(convention.nodes)
+      assert node.requirements == [&Bid.double/1]
+    end
+  end
+
+  describe "#redouble" do
+    test "returns the convention with a node with a redouble requirements" do
+      convention = Convention.redouble(Convention.new(:conv, "conv"))
+      node = hd(convention.nodes)
+      assert node.requirements == [&Bid.redouble/1]
     end
   end
 end

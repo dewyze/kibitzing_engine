535e37b480cbae850b27df7fd4b0ffab246d389f
John DeWyze <1312168+dewyze@users.noreply.github.com>
2019-11-02 22:15:44 -0500

    Enable processing of nodes

--- lib/kibitzing/engine/convention/node.ex
+++ lib/kibitzing/engine/convention/node.ex
@@ -1,7 +1,38 @@
 defmodule Kibitzing.Engine.Convention.Node do
-  defstruct [:method, :requirements]
+  defstruct [:method, :requirements, :nodes]
 
-  def new(method, requirements, _options \\ Keyword.new()) do
-    %__MODULE__{method: method, requirements: requirements}
+  alias Kibitzing.Engine.Models.Table
+
+  def new(method, objs, _options \\ Keyword.new()) do
+    case method do
+      :bid ->
+        %__MODULE__{method: :bid, requirements: objs}
+
+      :one_of ->
+        %__MODULE__{method: :one_of, nodes: objs}
+    end
+  end
+
+  def process(table, %__MODULE__{method: :bid, requirements: reqs}) do
+    %Table{bid: bid, previous_bids: prev, next_bids: next} = table
+
+    if Enum.all?(reqs, fn req -> req.(table) end) do
+      case next do
+        [] ->
+          {:ok, %Table{previous_bids: [bid | prev], bid: nil, next_bids: []}}
+
+        [h | t] ->
+          {:ok, %Table{previous_bids: [bid | prev], bid: h, next_bids: t}}
+      end
+    else
+      {:fail, table}
+    end
+  end
+
+  def process(table, %__MODULE__{method: :one_of, nodes: nodes}) do
+    Enum.find_value(nodes, {:fail, table}, fn node ->
+      result = process(table, node)
+      match?({:ok, _}, result) && result
+    end)
   end
 end
--- test/kibitzing/engine/convention/node_test.exs
+++ test/kibitzing/engine/convention/node_test.exs
@@ -2,6 +2,65 @@ defmodule Kibitzing.Engine.Convention.Requirement.NodeTest do
   use ExUnit.Case
   use ExUnitProperties
   # doctest Kibitzing.Engine.Convention.Node
-  alias Kibitzing.Engine.{Convention, Convention.Node}
-  alias Kibitzing.Engine.Convention.Requirement.Bid
+  alias Support.Generators, as: Gen
+  alias Kibitzing.Engine.Convention.Node
+  alias Kibitzing.Engine.Models.Table
+
+  describe "#process - bid" do
+    test "it returns {:ok, table} for a successful requirement" do
+      check all(table <- Gen.table()) do
+        req = fn _table -> true end
+        node = %Node{requirements: [req], method: :bid}
+        assert match?({:ok, %Table{}}, Node.process(table, node))
+      end
+    end
+
+    test "it returns {:fail, table} for a failed requirement" do
+      check all(table <- Gen.table()) do
+        req = fn _table -> false end
+        node = %Node{requirements: [req], method: :bid}
+        assert match?({:fail, %Table{}}, Node.process(table, node))
+      end
+    end
+
+    test "it moves the main bid to previous bids" do
+      check all(%Table{bid: bid, previous_bids: prev} = table <- Gen.table()) do
+        req = fn _table -> true end
+        node = %Node{requirements: [req], method: :bid}
+
+        assert match?(
+                 {:ok, %Table{previous_bids: [^bid | ^prev]}},
+                 Node.process(table, node)
+               )
+      end
+    end
+
+    test "it moves the first next bid to the main bid" do
+      check all(
+              %Table{next_bids: [h | t]} = table <-
+                Gen.table(next: list_of(Gen.bid(), min_length: 1))
+            ) do
+        req = fn _table -> true end
+        node = %Node{requirements: [req], method: :bid}
+
+        assert match?(
+                 {:ok, %Table{next_bids: ^t, bid: ^h}},
+                 Node.process(table, node)
+               )
+      end
+    end
+  end
+
+  describe "#process - one_of" do
+    test "it returns {:ok, table} if one node is successful" do
+      check all(table <- Gen.table()) do
+        true_req = fn _table -> true end
+        false_req = fn _table -> false end
+        true_node = %Node{requirements: [true_req], method: :bid}
+        false_node = %Node{requirements: [false_req], method: :bid}
+        node = %Node{method: :one_of, nodes: [false_node, true_node]}
+        assert match?({:ok, %Table{}}, Node.process(table, node))
+      end
+    end
+  end
 end
